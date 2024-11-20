7ed465b65d691fa0f71fe8f5b54a7bb99afde5f1
liuyunhe <liuyunhe666@aliyun.com>
2024-11-20 09:54:38 +0800

    refactor(index): 优化导出结构并移除未使用的 mdxPlugin
    - 移除了未使用的 mdxPlugin 导出
    - 保留了 Framework 的导出
    - 简化了 Vue 3 相关的导出语句

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import viteMdx, { Framework } from "./vite-mdx";
 
-const mdxPlugin = viteMdx
-
 export * from './vue3'
 
-export { Framework, mdxPlugin }
+export { Framework }
 
 export default viteMdx;
