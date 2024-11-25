45d59da7255a383e6a2a4f2b27ff21e06857fae7
liuyunhe <liuyunhe666@aliyun.com>
2024-11-25 11:33:27 +0800

    refactor(demo): 重构组件定义方式
    - 将组件定义从 App.tsx 中移出，创建独立的 components 文件夹
    - 新增 components/index.ts 文件，集中导出所有自定义组件
    - 删除 App.tsx 中的重复组件定义代码

--- demo/App.tsx
+++ demo/App.tsx
@@ -3,14 +3,8 @@ import { defineComponent } from 'vue';
 import Demo from './Demo.mdx'
 import { MDXProvider } from 'vite-mdx/vue3'
 
-import h1 from './components/h1'
+import components from './components';
 
-const components = {
-  h1: h1,
-  h2: () => <h2>h2</h2>,
-  h3: () => <h3>h3</h3>,
-  h4: () => <h4>h4</h4>
-}
 
 export default defineComponent({
   name: 'App',
